66797ed90dad15e0c089773b650ac4ac3cba0403
jim winstead <jimw@trainedmonkey.com>
2018-09-02 16:04:56 -0700

    Use domains.txt, create as needed

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p /var/www/dehydrated
 exec /srv/dehydrated/dehydrated -c --challenge "dns-01" \
-       --domain "${MUCK_HOST} ordure.${MUCK_HOST} scat.${MUCK_HOST}" \
        --hook /srv/dehydrated/dehydrated.default.sh \
-       --out /app/certs --config /app/config
+       --config /app/config \
+       --out /app/certs
