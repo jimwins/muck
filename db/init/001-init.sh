ca566ce4bece4e89afab81b63683b2c69af5ba84
jim winstead <jimw@trainedmonkey.com>
2018-12-29 18:23:02 -0800

    Remove talapoin stuff

--- db/init/001-init.sh
+++ db/init/001-init.sh
@@ -2,8 +2,6 @@
 set -e
 mysqladmin -uroot -p${MYSQL_ROOT_PASSWORD} create scat
 mysqladmin -uroot -p${MYSQL_ROOT_PASSWORD} create ordure
-mysqladmin -uroot -p${MYSQL_ROOT_PASSWORD} create talapoin
 mysql -uroot -p${MYSQL_ROOT_PASSWORD} \
       -e "GRANT ALL PRIVILEGES ON scat.* TO '${MYSQL_USER}'@'%';" \
-      -e "GRANT ALL PRIVILEGES ON ordure.* TO '${MYSQL_USER}'@'%';" \
-      -e "GRANT ALL PRIVILEGES ON talapoin.* TO '${MYSQL_USER}'@'%';"
+      -e "GRANT ALL PRIVILEGES ON ordure.* TO '${MYSQL_USER}'@'%';"
